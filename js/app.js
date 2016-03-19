a38609133ebee52d72053a381167108724106d07
Herman Chan <hermchan89@gmail.com>
2016-03-19 15:33:43 -0700

    X's and O's worked

--- js/app.js
+++ js/app.js
@@ -2,16 +2,15 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  $(this).find(".box").click( function (event){
-    var i = 0;
-    for (i = 0; i < 9; i ++)
-    if ($(this).text() === ""){
-      $(this).text("X");
-    } else if ($(this).text() === "") {
-        $(this).text("O");
-    } else if ($(this).text() === "") {
-      ($(this).text() === "X");
-    }
 
-});
+var turnCount = 0;
+
+  $(this).find(".box").click( function (event){
+    if (turnCount % 2 === 0){
+       $(this).text("X");
+    } else {
+       $(this).text("O");
+    }
+   turnCount ++;
+  });
 });
