02fbad6f6c1f0aee94e5af175d2ce4e9ec45acc0
Herman Chan <hermchan89@gmail.com>
2016-03-18 23:00:22 -0700

    started tic tac toe

--- js/app.js
+++ js/app.js
@@ -2,5 +2,16 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  $(this).find(".box").click( function (event){
+    var i = 0;
+    for (i = 0; i < 9; i ++)
+    if ($(this).text() === ""){
+      $(this).text("X");
+    } else if ($(this).text() === "") {
+        $(this).text("O");
+    } else if ($(this).text() === "") {
+      ($(this).text() === "X");
+    }
 
+});
 });
